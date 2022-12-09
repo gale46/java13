d7bd20a8841d4d7032ff282f68820020a43620df
gale46 <113490808+gale46@users.noreply.github.com>
2022-12-09 15:53:11 +0800

    Create class02.java

---
+++ class02.java
@@ -0,0 +1,14 @@
+// app13_1, ���ޭȶW�X�d��
+public class app13_1 {
+   public static void main(String args[]) {
+      int num = 12, den = 0, ans;
+      if (den == 0) {
+         System.out.println("除數為0");
+      } else {
+         ans = num / den;
+         System.out.println(ans);
+      }
+      // ���ޭȶW�X�e�\�d��
+
+   }
+}
